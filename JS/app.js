9410ca0c53168cfbcf18d50d359d4f7807fa4a7f
joydeep-git <joydeep.accounts@tuta.io>
2022-05-17 18:45:32 +0530

    updated

--- JS/app.js
+++ JS/app.js
@@ -6,6 +6,17 @@ const confirmpassword = document.getElementById('confirm-password');
 const number = document.getElementById('number');
 const message = document.getElementById('message');
 
+
+const showError = function(input, message){
+    let formControl = input.parentElement;
+    formControl.classlist = 'form-control error';
+    const small = formControl.querySelector('small')
+    small.innerText = message;
+}
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
+    if (username.value.trim() === ''){
+        showError(username, 'username kaun dega, tera baap?')
+    }
 })
