c137447aa6de075fe78acfd10f1f1047ee2909cd
joydeep-git <joydeep.accounts@tuta.io>
2022-05-17 19:55:04 +0530

    error color showing

--- JS/app.js
+++ JS/app.js
@@ -4,12 +4,12 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const confirmpassword = document.getElementById('confirm-password');
 const number = document.getElementById('number');
-const message = document.getElementById('message');
+const review = document.getElementById('review');
 
 
 const showError = function(input, message){
     let formControl = input.parentElement;
-    formControl.classlist = 'form-control error';
+    formControl.classList = 'form-control error';
     const small = formControl.querySelector('small')
     small.innerText = message;
 }
@@ -17,6 +17,21 @@ const showError = function(input, message){
 form.addEventListener('submit', function(e){
     e.preventDefault();
     if (username.value.trim() === ''){
-        showError(username, 'username kaun dega, tera baap?')
+        showError(username, 'Username is mandatory')
+    }
+    if (email.value.trim() === ''){
+        showError(email, 'email is mandatory')
+    }
+    if (password.value.trim() === ''){
+        showError(password, 'password is mandatory')
+    }
+    if (confirmpassword.value.trim() === ''){
+        showError(confirmpassword, 'confirming your password is mandatory')
+    }
+    if (number.value.trim() === ''){
+        showError(number, 'number is mandatory')
+    }
+    if (review.value.trim() === ''){
+        showError(review, 'write your thoughts about us')
     }
 })
