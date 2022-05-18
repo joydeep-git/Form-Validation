cc96ce9a6a85284799388b16912e9dedaf26b5f4
joydeep-git <joydeep.accounts@tuta.io>
2022-05-18 23:07:20 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -14,24 +14,41 @@ const showError = function(input, message){
     small.innerText = message;
 }
 
+const showSuccess = function(input){
+    let formControl = input.parentElement;
+    formControl.classList = 'form-control success';
+}
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
     if (username.value.trim() === ''){
         showError(username, 'Username is mandatory')
+    }else{
+        showSuccess(username)
     }
     if (email.value.trim() === ''){
         showError(email, 'email is mandatory')
+    }else{
+        showSuccess(email)
     }
     if (password.value.trim() === ''){
         showError(password, 'password is mandatory')
+    }else{
+        showSuccess(password)
     }
     if (confirmpassword.value.trim() === ''){
         showError(confirmpassword, 'confirming your password is mandatory')
+    }else{
+        showSuccess(confirmpassword)
     }
     if (number.value.trim() === ''){
         showError(number, 'number is mandatory')
+    }else{
+        showSuccess(number)
     }
     if (review.value.trim() === ''){
         showError(review, 'write your thoughts about us')
+    }else{
+        showSuccess(review)
     }
 })
