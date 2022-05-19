41a986e86fb46ac468d05408f29b2ba68fda4cfc
joydeep-git <joydeep.accounts@tuta.io>
2022-05-19 18:13:23 +0530

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -6,49 +6,67 @@ const confirmpassword = document.getElementById('confirm-password');
 const number = document.getElementById('number');
 const review = document.getElementById('review');
 
+const inputArr = [username, email, password, confirmpassword, number, review];
 
 const showError = function(input, message){
-    let formControl = input.parentElement;
+    const formControl = input.parentElement;
     formControl.classList = 'form-control error';
     const small = formControl.querySelector('small')
     small.innerText = message;
 }
 
 const showSuccess = function(input){
-    let formControl = input.parentElement;
+    const formControl = input.parentElement;
     formControl.classList = 'form-control success';
 }
 
-form.addEventListener('submit', function(e){
+const checkRequired = function(inputArr){
+    inputArr.forEach((input) => {
+        if (input.value === '') {
+            showError(input, `${input} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+};
+
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    if (username.value.trim() === ''){
-        showError(username, 'Username is mandatory')
-    }else{
-        showSuccess(username)
-    }
-    if (email.value.trim() === ''){
-        showError(email, 'email is mandatory')
-    }else{
-        showSuccess(email)
-    }
-    if (password.value.trim() === ''){
-        showError(password, 'password is mandatory')
-    }else{
-        showSuccess(password)
-    }
-    if (confirmpassword.value.trim() === ''){
-        showError(confirmpassword, 'confirming your password is mandatory')
-    }else{
-        showSuccess(confirmpassword)
-    }
-    if (number.value.trim() === ''){
-        showError(number, 'number is mandatory')
-    }else{
-        showSuccess(number)
-    }
-    if (review.value.trim() === ''){
-        showError(review, 'write your thoughts about us')
-    }else{
-        showSuccess(review)
-    }
-})
+    checkRequired(inputArr)
+});
+
+// form.addEventListener('submit', function(e){
+//     e.preventDefault();
+
+
+//     if (username.value.trim() === ''){
+//         showError(username, 'Username is mandatory')
+//     }else{
+//         showSuccess(username)
+//     }
+//     if (email.value.trim() === ''){
+//         showError(email, 'email is mandatory')
+//     }else{
+//         showSuccess(email)
+//     }
+//     if (password.value.trim() === ''){
+//         showError(password, 'password is mandatory')
+//     }else{
+//         showSuccess(password)
+//     }
+//     if (confirmpassword.value.trim() === ''){
+//         showError(confirmpassword, 'confirming your password is mandatory')
+//     }else{
+//         showSuccess(confirmpassword)
+//     }
+//     if (number.value.trim() === ''){
+//         showError(number, 'number is mandatory')
+//     }else{
+//         showSuccess(number)
+//     }
+//     if (review.value.trim() === ''){
+//         showError(review, 'write your thoughts about us')
+//     }else{
+//         showSuccess(review)
+//     }
+// })
